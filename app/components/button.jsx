d8230f10f0eec281e47696048a1ce77e9846d207
adammertel <mertel.adam@gmail.com>
2017-07-21 09:21:25 +0200

    button component

---
+++ app/components/button.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default class Button extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    
+    return (
+      <a 
+        className={'button is-small is-primary ' + this.props.className} 
+        onClick={this.props.onClick}
+        style={this.props.style}
+      >
+        {
+          this.props.icon ? (
+            <span className="icon is-small">
+              <i className={'fa fa-' + this.props.icon}></i>
+            </span>
+          ) : null
+        }
+        {
+          this.props.label ? (
+            <span>{this.props.label}</span>
+          ) : null
+        }
+      </a>
+    )
+  }
+}
