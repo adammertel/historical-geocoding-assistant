b4dcef3b5c1858b20167be103a23e7ab511376ef
adammertel <mertel.adam@gmail.com>
2017-07-21 10:49:39 +0200

    input  component

---
+++ app/components/input.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class Input extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    
+    return (
+      <div className="field">
+        <div className="control">
+          <input 
+            className="input is-primary" 
+            type="text" 
+            value={this.props.value} 
+          />
+        </div>
+      </div>
+    )
+  }
+}
