b5dd3f4adfebe7057f4231409b3fb0ce39c27ceb
adammertel <mertel.adam@gmail.com>
2017-10-26 22:54:16 +0200

    switch component

---
+++ app/components/switch.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default class Switch extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  style () {
+    return {
+      fontSize: 11
+    }
+  }
+
+  render() {
+    return (
+      <div className="field">
+        <input 
+          id={this.props.id} 
+          type="checkbox" 
+          name={this.props.id}
+          className={'switch ' + this.props.classes} 
+          checked={this.props.checked} />
+        <label htmlFor={this.props.id}>{this.props.label}</label>
+      </div>
+    )
+  }
+}
