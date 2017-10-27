93c7980ccd303cf675781050788482cac6518f72
adammertel <mertel.adam@gmail.com>
2017-10-27 12:13:29 +0200

    message component

---
+++ app/components/message.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default class Message extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  style () {
+    return {
+      fontSize: 11
+    }
+  }
+
+  render() {
+    return (
+      <article className="message">
+      <div className={'message-header' + this.props.classes}>
+        <p>{this.props.header}</p>
+        <button className="delete" ariaLabel="delete"></button>
+      </div>
+      <div className="message-body">
+      {
+        this.props.children
+      }
+      </div>
+      </article>
+    )
+  }
+}
