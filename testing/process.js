710b58d14714aa1332ec2232581d90e028e8a816
adammertel <mertel.adam@gmail.com>
2018-06-15 00:59:02 +0200

    new tests

--- testing/process.js
+++ testing/process.js
@@ -1,23 +1,21 @@
-var csv = require('csv-parser');
-var fs = require('fs');
-var turf = require('turf');
-const Json2csvParser = require('json2csv').Parser;
+var csv = require("csv-parser");
+var fs = require("fs");
+var turf = require("turf");
+const Json2csvParser = require("json2csv").Parser;
 
 var coded = [];
 var records = [];
 var users = [];
 
-fs
-  .createReadStream('results - coded.csv')
+fs.createReadStream("results - coded.csv")
   .pipe(csv())
-  .on('data', data => {
+  .on("data", data => {
     coded.push(data);
   })
-  .on('end', () => {
-    fs
-      .createReadStream('results - records.csv')
+  .on("end", () => {
+    fs.createReadStream("results - records.csv")
       .pipe(csv())
-      .on('data', data => {
+      .on("data", data => {
         data.Precision_HGA = 0;
         data.Precision_Manual = 0;
 
@@ -32,14 +30,13 @@ fs
 
         records.push(data);
       })
-      .on('end', () => {
-        fs
-          .createReadStream('results - users.csv')
+      .on("end", () => {
+        fs.createReadStream("results - users.csv")
           .pipe(csv())
-          .on('data', data => {
+          .on("data", data => {
             users.push(data);
           })
-          .on('end', () => {
+          .on("end", () => {
             // own code
 
             coded.filter(c => c).map(code => {
@@ -51,25 +48,19 @@ fs
                 parseFloat(code.x_coordinates),
                 parseFloat(code.y_coordinates)
               ]);
-              const HGA = code.HGA === 'TRUE';
+              const HGA = code.HGA === "TRUE";
               const distance = parseInt(
                 turf.distance(record.geo, codedPoint),
                 10
               );
 
-              // add time
-              const time = parseInt(code['Time [s]'], 10);
-              record.Time_All += time;
-              if (HGA) {
-                record.Time_HGA += time;
-              } else {
-                record.Time_Manual += time;
-              }
-
               // wrong geolocalisation
-              const maxDistance = code.Certainty === '1' ? 15 : 50;
-              const precisionPoints = code.Certainty === '1' ? 1 : 0.5;
-              if (distance < maxDistance) {
+              const maxDistance = code.Certainty === "1" ? 15 : 50;
+              const precisionPoints = code.Certainty === "1" ? 1 : 0.5;
+
+              const ok = distance < maxDistance;
+
+              if (ok) {
                 if (HGA) {
                   record.Precision_HGA += precisionPoints;
                 } else {
@@ -78,17 +69,26 @@ fs
               } else {
                 console.log(distance, code.Id, code);
               }
+
+              // add time
+              if (ok) {
+                const time = parseInt(code["Time [s]"], 10);
+                record.Time_All += time;
+                if (HGA) {
+                  record.Time_HGA += time;
+                } else {
+                  record.Time_Manual += time;
+                }
+              }
             });
 
             records.map(r => {
-              r.Time_HGA = r.Time_HGA / 4;
-              r.Time_Manual = r.Time_Manual / 4;
-              r.Precision_HGA = r.Precision_HGA / 4;
-              r.Precision_Manual = r.Precision_Manual / 4;
-              r.Time_All = r.Time_All / 8;
+              r.Time_HGA = r.Time_HGA / r.Precision_HGA;
+              r.Time_Manual = r.Time_Manual / r.Precision_Manual;
+              r.Time_All = r.Time_All / (r.Precision_Manual + r.Precision_HGA);
             });
 
-            const timeThresholds = [130, 200];
+            const timeThresholds = [130, 250];
             records.map(r => {
               let timeCategory = 3;
               if (r.Time_All < timeThresholds[1]) {
@@ -102,7 +102,9 @@ fs
 
             const json2csvParser = new Json2csvParser(Object.keys(records));
             const out = json2csvParser.parse(records);
-            fs.writeFile('out.csv', out);
+            fs.writeFile("out.csv", out, () => {
+              console.log("outfile saved");
+            });
           });
       });
   });
