b346976eefe35c4f75c1890fa51e60ff1a923945
adammertel <mertel.adam@gmail.com>
2018-06-16 19:28:03 +0200

    vega lite test

--- testing/process.js
+++ testing/process.js
@@ -33,11 +33,21 @@ fs.createReadStream("results - coded.csv")
 
         records.push(data);
       })
+
       .on("end", () => {
+        // loading users.csv
         fs.createReadStream("results - users.csv")
           .pipe(csv())
-          .on("data", data => {
-            users.push(data);
+          .on("data", uData => {
+            uData.Precision_Manual = 0;
+            uData.Precision_HGA = 0;
+            uData.Time_HGA = 0;
+            uData.Time_Manual = 0;
+            uData.Time_All = 0;
+            uData.Correct_Manual = 0;
+            uData.Correct_HGA = 0;
+            uData.Correct_All = 0;
+            users.push(uData);
           })
           .on("end", () => {
             // own code
@@ -45,12 +55,17 @@ fs.createReadStream("results - coded.csv")
             coded.filter(c => c).map(code => {
               const record = records.find(r => r.Id === code.Id);
 
-              console.log(code.Id);
+              // user that coded this record
+              const user = users.find(u => u.ID === code.User);
+              code.User_Geo = user.Geocoding;
+              code.User_Topic = user.Topic;
+              code.Time = parseInt(code["Time [s]"], 10);
 
               const codedPoint = turf.point([
                 parseFloat(code.x_coordinates),
                 parseFloat(code.y_coordinates)
               ]);
+
               const HGA = code.HGA === "TRUE";
               const distance = parseInt(
                 turf.distance(record.geo, codedPoint),
@@ -62,28 +77,40 @@ fs.createReadStream("results - coded.csv")
               const precisionPoints = code.Certainty === "1" ? 1 : 0.5;
 
               const ok = distance < maxDistance;
+              code.Distance = distance;
 
               if (ok) {
+                code.Accurate = precisionPoints;
                 if (HGA) {
                   record.Precision_HGA += precisionPoints;
+                  user.Precision_HGA += precisionPoints;
                 } else {
                   record.Precision_Manual += precisionPoints;
+                  user.Precision_Manual += precisionPoints;
                 }
               } else {
+                code.Accurate = 0;
                 console.log(distance, code.Id, code);
               }
 
               // add time
               if (ok) {
-                const time = parseInt(code["Time [s]"], 10);
+                const time = code.Time;
                 record.Time_All += time;
                 record.Correct_All += 1;
+                user.Time_All += time;
+                user.Correct_All += 1;
+
                 if (HGA) {
                   record.Time_HGA += time;
                   record.Correct_HGA += 1;
+                  user.Time_HGA += time;
+                  user.Correct_HGA += 1;
                 } else {
                   record.Time_Manual += time;
                   record.Correct_Manual += 1;
+                  user.Time_Manual += time;
+                  user.Correct_Manual += 1;
                 }
               }
             });
@@ -99,6 +126,23 @@ fs.createReadStream("results - coded.csv")
               r.Precision_Manual = r.Precision_Manual / 6;
             });
 
+            users.map(r => {
+              r.Precision_HGA = r.Precision_HGA / 10;
+              r.Precision_Manual = r.Precision_Manual / 10;
+            });
+
+            users.map(r => {
+              r.Time_HGA = r.Time_HGA / r.Correct_HGA;
+              r.Time_Manual = r.Time_Manual / r.Correct_Manual;
+              r.Time_All = r.Time_All / r.Correct_All;
+            });
+
+            coded.map(c => {
+              const record = records.find(r => r.Id === c.Id);
+              c.Record_Avg = record.Time_All;
+              c.Record_Difficulty = record.Time_All > 150 ? 1 : 0;
+            });
+
             const timeThresholds = [150];
             records.map(r => {
               let timeCategory = 2;
@@ -108,11 +152,46 @@ fs.createReadStream("results - coded.csv")
               r.Time_Category = timeCategory;
             });
 
-            const json2csvParser = new Json2csvParser(Object.keys(records));
-            const out = json2csvParser.parse(records);
-            fs.writeFile("difficulty.csv", out, () => {
+            const parserRecords = new Json2csvParser(Object.keys(records));
+            const diffOut = parserRecords.parse(records);
+
+            fs.writeFile("out_difficulty.csv", diffOut, () => {
               console.log("diffuculty output saved");
             });
+
+            const parserUsers = new Json2csvParser(Object.keys(users));
+            const usersOut = parserUsers.parse(users);
+
+            fs.writeFile("out_users.csv", usersOut, () => {
+              console.log("users output saved");
+            });
+
+            const parserCoded = new Json2csvParser(Object.keys(coded));
+            const codedOut = parserCoded.parse(coded);
+
+            fs.writeFile("out_coded.csv", codedOut, () => {
+              console.log("coded output saved");
+            });
+
+            ["Time [s]"];
+
+            // do json from coded
+            fs.writeFile(
+              "coded.js",
+              "var data = " + JSON.stringify({ values: coded }),
+              () => {
+                console.log("coded.js output saved");
+              }
+            );
+
+            // do json from records
+            fs.writeFile(
+              "records.js",
+              "var data = " + JSON.stringify({ values: records }),
+              () => {
+                console.log("records.js output saved");
+              }
+            );
           });
       });
   });
