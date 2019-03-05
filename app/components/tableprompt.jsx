b04db22958d686a2ef7a3b857288f024380b09a9
adammertel <mertel.adam@gmail.com>
2019-03-05 14:37:51 +0100

    cookie in the prompt

--- app/components/tableprompt.jsx
+++ app/components/tableprompt.jsx
@@ -1,42 +1,43 @@
-import React from 'react'
-import { observer } from 'mobx-react'
-import Modal from './../bulma/modal'
-import Input from './../bulma/input'
-import Button from './../bulma/button'
+import React from "react";
+import { observer } from "mobx-react";
+import Modal from "./../bulma/modal";
+import Input from "./../bulma/input";
+import Button from "./../bulma/button";
 
-const logoPath = require('./../assets/logo.png')
+const logoPath = require("./../assets/logo.png");
 
-@observer class TablePrompt extends React.Component {
-  constructor (props) {
-    super(props)
+@observer
+class TablePrompt extends React.Component {
+  constructor(props) {
+    super(props);
     this.state = {
-      sheetId: ''
-    }
-    this.exampleSheetId = '1FaW23x-ZT3pmdmv77eKPJxsfGhoB1urwfvPffN_4keU'
+      sheetId: ""
+    };
+    this.exampleSheetId = "1FaW23x-ZT3pmdmv77eKPJxsfGhoB1urwfvPffN_4keU";
   }
 
-  changeSheetId (e) {
-    this.setState({ sheetId: e.target.value })
+  changeSheetId(e) {
+    this.setState({ sheetId: e.target.value });
   }
 
-  acceptSheetId () {
-    sheetId = this.state.sheetId
-    location.hash = sheetId
-    initSheet()
+  acceptSheetId() {
+    sheetId = this.state.sheetId;
+    location.hash = sheetId;
+    initSheet();
   }
 
-  acceptExampleSheetId () {
-    console.log(this.exampleSheetId)
-    sheetId = this.exampleSheetId
-    location.hash = sheetId
-    initSheet()
+  acceptExampleSheetId() {
+    console.log(this.exampleSheetId);
+    sheetId = this.exampleSheetId;
+    location.hash = sheetId;
+    initSheet();
   }
 
-  render () {
+  render() {
     return (
       <Modal
         active
-        classes='is-primary'
+        classes="is-primary"
         style={{ zIndex: 1500 }}
         body={
           <div
@@ -48,39 +49,39 @@ const logoPath = require('./../assets/logo.png')
           >
             <img
               src={logoPath}
-              alt='logo'
+              alt="logo"
               style={{
-                width: '70%',
-                margin: 'auto',
-                left: '10%',
+                width: "70%",
+                margin: "auto",
+                left: "10%",
                 padding: 10,
-                position: 'relative'
+                position: "relative"
               }}
             />
             <div style={{ padding: 30 }}>
               Enter the ID number of your Google Sheet (
               <a
-                href='https://developers.google.com/sheets/api/guides/concepts'
-                target='_blank'
+                href="https://developers.google.com/sheets/api/guides/concepts"
+                target="_blank"
               >
                 what is a Google Sheet ID?
-              </a>{' '}
+              </a>{" "}
               ):
               <Input
                 value={this.state.sheetId}
                 onChange={this.changeSheetId.bind(this)}
               />
-              Or use the{' '}
+              Or use the{" "}
               <a
-                target='_blank'
-                href='https://docs.google.com/spreadsheets/d/1FaW23x-ZT3pmdmv77eKPJxsfGhoB1urwfvPffN_4keU'
+                target="_blank"
+                href="https://docs.google.com/spreadsheets/d/1FaW23x-ZT3pmdmv77eKPJxsfGhoB1urwfvPffN_4keU"
               >
                 example sheet
-              </a>{' '}
-              id : <br />{' '}
+              </a>{" "}
+              id : <br />{" "}
               <Button
                 onClick={this.acceptExampleSheetId.bind(this)}
-                label={'example sheet'}
+                label={"example sheet"}
               />
               <br />
               <br />
@@ -88,35 +89,36 @@ const logoPath = require('./../assets/logo.png')
               <b>Notes:</b>
               <br />
               - The application has to be signed into a Google Account (you may
-              need to allow pop-ups)
+              need to allow pop-ups) and the browser needs to allow cookies.
               <br />
-              - You can use the testing google account <br />(username:{' '}
-              <b>historicalgeocodingassistant</b>, password: <b>hga54321</b>)
+              - You can use the testing google account <br />
+              (username: <b>historicalgeocodingassistant</b>, password:{" "}
+              <b>hga54321</b>)
               <br />
               <br />
               <b>Other links:</b>
               <br />
-              <a href='https://github.com/adammertel/historical-geocoder-assistant'>
+              <a href="https://github.com/adammertel/historical-geocoder-assistant">
                 - Code at Github
               </a>
               <br />
-              <a href='https://github.com/adammertel/historical-geocoder-assistant/tree/master/manual'>
+              <a href="https://github.com/adammertel/historical-geocoder-assistant/tree/master/manual">
                 - Documentation
               </a>
             </div>
           </div>
         }
-        footerStyle={{ textAlign: 'right', display: 'block' }}
+        footerStyle={{ textAlign: "right", display: "block" }}
         footer={
           <Button
             medium
             onClick={this.acceptSheetId.bind(this)}
-            label={'continue'}
+            label={"continue and accept cookies"}
           />
         }
       />
-    )
+    );
   }
 }
 
-export default observer(TablePrompt)
+export default observer(TablePrompt);
--- app/sheet.js
+++ app/sheet.js
@@ -1,37 +1,39 @@
 var Sheet = {
-  scope: 'https://www.googleapis.com/auth/spreadsheets', // 'https://www.googleapis.com/auth/spreadsheets',
+  scope: "https://www.googleapis.com/auth/spreadsheets", // 'https://www.googleapis.com/auth/spreadsheets',
   header: [],
 
   noLines: 999999,
-  noColumns: 'ZZ',
+  noColumns: "ZZ",
 
   auth: false,
 
-  init (next) {
-    this.clientId = config.clientId
-    this.apiKey = config.apiKey
-    this.sId = sheetId
+  init(next) {
+    this.clientId = config.clientId;
+    this.apiKey = config.apiKey;
+    this.sId = sheetId;
     this._authentificate(() => {
       this._pingTable(pinged => {
         if (pinged) {
-          window[
-            'username'
-          ] = gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile().getName()
+          window["username"] = gapi.auth2
+            .getAuthInstance()
+            .currentUser.get()
+            .getBasicProfile()
+            .getName();
 
-          console.log('username', username)
+          console.log("username", username);
 
-          this._preRead(next)
+          this._preRead(next);
         } else {
-          alert('table was not ititialized, wrong id')
-          location.reload()
+          alert("table was not ititialized, wrong id");
+          location.reload();
         }
-      })
-    })
+      });
+    });
   },
 
-  _authentificate (next) {
-    gapi.load('client:auth2', () => {
-      store.changeLoadingStatus('table')
+  _authentificate(next) {
+    gapi.load("client:auth2", () => {
+      store.changeLoadingStatus("table");
       // gapi.client.setApiKey(this.apiKey);
 
       gapi.auth2
@@ -40,220 +42,225 @@ var Sheet = {
           scope: this.scope
         })
         .then(() => {
-          this.auth = gapi.auth2.getAuthInstance()
+          this.auth = gapi.auth2.getAuthInstance();
 
-          console.log
+          console.log;
 
           if (this.auth.isSignedIn.get()) {
-            next()
+            next();
           } else {
             alert(
-              'Application needs to be signed in - please enable pop-ups and reload the page to log in'
-            )
+              "Application needs to be signed in - please enable pop-ups and reload the page to log in"
+            );
 
             const signedStateChanged = () => {
-              console.log('should be signed in now')
-              next()
-            }
-            this.auth.isSignedIn.listen(signedStateChanged)
-            this.auth.signIn()
+              console.log("should be signed in now");
+              next();
+            };
+            this.auth.isSignedIn.listen(signedStateChanged);
+            this.auth.signIn();
           }
-        })
-    })
+        });
+    });
   },
 
-  _ensureAuthentificated (next) {
+  _ensureAuthentificated(next) {
     if (this.auth) {
-      this.auth.isSignedIn.get() ? next() : this._authentificate(next)
+      this.auth.isSignedIn.get() ? next() : this._authentificate(next);
     } else {
-      this._authentificate(next)
+      this._authentificate(next);
     }
   },
 
-  _preRead (next) {
+  _preRead(next) {
     this._checkColumns(() => {
       this._checkRows(() => {
-        this._loadHeader(next)
-      })
-    })
+        this._loadHeader(next);
+      });
+    });
   },
 
-  _pingTable (next) {
+  _pingTable(next) {
     this.readLine(1, true, pingedData => {
       if (pingedData) {
-        next(true)
+        next(true);
       } else {
-        next(false)
+        next(false);
       }
-    })
+    });
   },
 
-  _checkColumns (next) {
+  _checkColumns(next) {
     this.readLine(1, true, headerData => {
       if (headerData) {
         try {
-          this.noColumns = headerData.result.range.split('!')[1].split(':')[1].split('1')[
-            0
-          ]
+          this.noColumns = headerData.result.range
+            .split("!")[1]
+            .split(":")[1]
+            .split("1")[0];
         } catch (e) {
-          console.log('error reading columns', e)
+          console.log("error reading columns", e);
         }
       }
-      next()
-    })
+      next();
+    });
   },
 
-  _checkRows (next) {
+  _checkRows(next) {
     this.readAllLines(rowsData => {
       if (rowsData) {
         try {
-          this.noLines = Math.max(...Object.keys(rowsData))
+          this.noLines = Math.max(...Object.keys(rowsData));
         } catch (e) {
-          console.log('error reading rows', e)
+          console.log("error reading rows", e);
         }
       }
-      next()
-    })
+      next();
+    });
   },
 
-  _loadHeader (next) {
+  _loadHeader(next) {
     this.readLine(1, true, headerData => {
       if (headerData) {
-        this.header = headerData.result.values[0]
+        this.header = headerData.result.values[0];
       }
 
-      next()
+      next();
       // console.log(this.header);
-    })
+    });
   },
 
-  _parseRow (row, withoutParsing) {
+  _parseRow(row, withoutParsing) {
     if (withoutParsing) {
-      return row
+      return row;
     } else {
       if (row && row.result && row.result.values) {
-        const data = {}
-        row.result.values[0].map((value, vi) => (data[this.header[vi]] = value))
-        return data
+        const data = {};
+        row.result.values[0].map(
+          (value, vi) => (data[this.header[vi]] = value)
+        );
+        return data;
       } else {
-        return row
+        return row;
       }
     }
   },
 
-  _parseRecords (response) {
-    const records = {}
+  _parseRecords(response) {
+    const records = {};
     response.result.values.map((row, i) => {
-      const rowColumns = {}
-      this.header.forEach((columnName, ci) => (rowColumns[columnName] = row[ci]))
-      records[i + 2] = rowColumns
-    })
+      const rowColumns = {};
+      this.header.forEach(
+        (columnName, ci) => (rowColumns[columnName] = row[ci])
+      );
+      records[i + 2] = rowColumns;
+    });
 
-    return records
+    return records;
   },
 
-  _readLineUrl (lineNo) {
-    const range = 'A' + lineNo + ':' + this.noColumns + lineNo
+  _readLineUrl(lineNo) {
+    const range = "A" + lineNo + ":" + this.noColumns + lineNo;
     return (
-      'https://sheets.googleapis.com/v4/spreadsheets/' +
+      "https://sheets.googleapis.com/v4/spreadsheets/" +
       this.sId +
-      '/values/' +
+      "/values/" +
       range +
-      '?key=' +
+      "?key=" +
       this.apiKey
-    )
+    );
   },
 
-  _updateLineUrl (lineNo) {
-    const range = 'A' + lineNo + ':' + this.noColumns + lineNo
+  _updateLineUrl(lineNo) {
+    const range = "A" + lineNo + ":" + this.noColumns + lineNo;
     return (
-      'https://sheets.googleapis.com/v4/spreadsheets/' +
+      "https://sheets.googleapis.com/v4/spreadsheets/" +
       this.sId +
-      '/values/' +
+      "/values/" +
       range +
-      '?key=' +
+      "?key=" +
       this.apiKey +
-      '&valueInputOption=RAW'
-    )
+      "&valueInputOption=RAW"
+    );
   },
 
-  _readAll () {
-    const range = 'A2:' + this.noColumns + this.noLines
+  _readAll() {
+    const range = "A2:" + this.noColumns + this.noLines;
     return (
-      'https://sheets.googleapis.com/v4/spreadsheets/' +
+      "https://sheets.googleapis.com/v4/spreadsheets/" +
       this.sId +
-      '/values/' +
+      "/values/" +
       range +
-      '?key=' +
+      "?key=" +
       this.apiKey
-    )
+    );
   },
 
-  _updateBody (data) {
+  _updateBody(data) {
     return {
       values: [data]
-    }
+    };
   },
 
-  readLine (lineNo, withoutParsing, next) {
+  readLine(lineNo, withoutParsing, next) {
     this._ensureAuthentificated(() => {
       gapi.client
         .request({
           path: this._readLineUrl(lineNo),
-          method: 'GET'
+          method: "GET"
         })
         .then(
           response => next(this._parseRow(response, withoutParsing)),
           response => {
-            this._reportError(response)
-            next(false)
+            this._reportError(response);
+            next(false);
           }
-        )
-    })
+        );
+    });
   },
 
-  readAllLines (next) {
+  readAllLines(next) {
     this._ensureAuthentificated(() => {
       gapi.client
         .request({
           path: this._readAll(),
-          method: 'GET'
+          method: "GET"
         })
         .then(
           response => {
-            next(this._parseRecords(response))
+            next(this._parseRecords(response));
           },
           response => {
-            this._reportError(response)
-            next(false)
+            this._reportError(response);
+            next(false);
           }
-        )
-    })
+        );
+    });
   },
 
-  updateLine (lineNo, data, next) {
+  updateLine(lineNo, data, next) {
     this._ensureAuthentificated(() => {
       gapi.client
         .request({
           path: this._updateLineUrl(lineNo),
-          method: 'PUT',
+          method: "PUT",
           body: this._updateBody(data)
         })
         .then(
           response => next(response.result.values),
           response => {
-            this._reportError(response)
-            next(false)
+            this._reportError(response);
+            next(false);
           }
-        )
-    })
+        );
+    });
   },
 
-  _reportError (errResponse) {
-    console.log('ERR')
-    console.log(errResponse)
-    console.log('ERR')
+  _reportError(errResponse) {
+    console.log("ERR");
+    console.log(errResponse);
+    console.log("ERR");
   }
-}
+};
 
-module.exports = Sheet
+module.exports = Sheet;
