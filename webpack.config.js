1bf89546173f1c15d09a5edca9b8627b8f4a0b8e
adammertel <mertel.adam@gmail.com>
2019-03-05 17:53:15 +0100

    packages updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,53 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: [".js", ".jsx"]
   },
-  entry: './app/main.jsx',
+
+  entry: "./app/main.jsx",
   module: {
     rules: [
       {
         test: /\.js$|\.jsx$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: "babel-loader"
         }
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: 'html-loader',
+            loader: "html-loader",
             options: { minimize: true }
           }
         ]
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
       },
       {
         test: /\.scss$/,
-        loaders: [
-          'style-loader',
-          { loader: 'css-loader', options: { importLoaders: 1 } },
-          'sass-loader'
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Nodea Sass by default
         ]
       },
+      { test: /\.(eot|ttf|svg|woff|woff2)$/, loader: "file-loader" },
       {
-        test: /\.(jpe?g|png|gif|svg|eot|ttf|svg|woff|woff2|ico)$/i,
+        test: /\.(jpe?g|png|gif|svg)$/i,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[ext]',
-              outputPath: './assets/',
-              publicPath: './assets/'
+              name: "[name].[ext]",
+              outputPath: "./assets/media/",
+              publicPath: "./assets/media/"
             }
           }
         ]
@@ -54,18 +56,19 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: './app/index.html',
-      filename: './index.html',
-      favicon: './app/assets/favicon.ico'
+      template: "./app/index.html",
+      filename: "./index.html",
+      favicon: "./app/assets/favicon.ico"
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css'
+      filename: "[name].css",
+      chunkFilename: "[id].css"
     }),
     new CopyWebpackPlugin(
       [
-        { from: './app/data', to: 'data' },
-        { from: './app/configs', to: 'configs' }
+        { from: "./app/assets", to: "assets" },
+        { from: "./app/data", to: "data" },
+        { from: "./app/configs", to: "configs" }
       ],
       {}
     )
