593e6e0684857b2a6b08b9f7a689f13e8037abda
adammmertel <mertel.adam@gmail.com>
2019-09-11 11:26:34 +0200

    :sparkles:FEAT: Pleiades suggestions, similarity index

--- app/suggestions.js
+++ app/suggestions.js
@@ -234,20 +234,35 @@ var SuggestionSources = [
       console.log(term);
       console.log(data["pleiades"]);
 
-      const ids = checkNoLimit(
-        data["pleiades"]
-          .filter(p => {
-            return p[0] === term;
-          })
-          .map(r => r[1])
-      );
+      let similars = [];
 
-      const all = ids.length;
+      data["pleiades"].forEach(p => {
+        const name = p[0];
+        const id = p[1];
+        let simScore = 0;
+        if (name === term) {
+          simScore = 1;
+        } else if (name.includes(term) || term.includes(name)) {
+          simScore = 0.8;
+        } else {
+          simScore = Base.simScore(name, term);
+        }
+        if (simScore > 0.4) {
+          similars.push({
+            id: id,
+            name: name,
+            sim: simScore
+          });
+        }
+      });
+
+      similars.sort((a, b) => (a.sim > b.sim ? -1 : 1));
+      similars = checkNoLimit(similars);
+      const all = similars.length;
       let processed = 0;
       const suggestions = [];
 
       const checkFinished = () => {
-        console.log("checking next", suggestions);
         if (processed === all) {
           next(mapRecords(suggestions, source.recordMap), false);
         }
@@ -256,11 +271,13 @@ var SuggestionSources = [
       // in case there are no suggestions
       checkFinished();
 
-      ids.forEach(id => {
+      similars.forEach(similar => {
+        const id = similar.id;
         const url = source.urls.record(id);
         Base.doFetch(url, {}, (err, res) => {
           processed++;
-          if (!err) {
+          if (!err && res.reprPoint) {
+            res.sim = similar.sim;
             suggestions.push(res);
           }
           checkFinished();
@@ -268,9 +285,12 @@ var SuggestionSources = [
       });
     },
     recordMap: {
-      ll: r => [r.reprPoint[1], r.reprPoint[0]],
+      ll: r => {
+        console.log(r);
+        return [r.reprPoint[1], r.reprPoint[0]];
+      },
       country: r => "",
-      rank: r => 100,
+      rank: r => r.sim,
       name: r => r.title,
       url: r => r.uri,
       type: r => r.placeTypes.join(),
